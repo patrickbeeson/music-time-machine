543f0d298a674fabbb5bf9d9add68bd165bbc430
Patrick Beeson <patrickbeeson@gmail.com>
2014-11-24 07:52:25 -0500

    Add analytics and event tracking for form submits.

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
     console.log("Thanks for exploring my diverse musical tastes!\n\nMy all-time top five musical artists are:\n\n* The Go! Team\n* Sufjan Stevens\n* The New Pornographers\n* Cannibal Ox\n* Boards of Canada\n\n- Patrick");
-
+   // Add event tracking to form submit
+    $('#form_submit').on('mousedown', function() {
+        // Build our event for GA
+        ga('send', 'event', 'form-submit', 'click');
+    });
 });
